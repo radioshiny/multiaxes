3a4cc3e031857cfd4d06d724d3a135dec86250b5
radioshiny <radioshiny@gmail.com>
2020-05-14 14:58:52 +0900

    Updating setup.py

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 `Multiaxes` is a python class based on `matplotlib`.  
 This script makes easy to draw a figure with multiple axes for the AASTex manuscript.
 
+## Install
+
+`pip install git+https://github.com/radioshiny/multiaxes.git`
+
 ## Examples
 
 ### Figure width
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="multiaxes", # Replace with your own username
+    version="0.0.1",
+    author="Shinyoung Kim",
+    author_email="radioshiny@gmail.com",
+    description="Multiaxes is a python class based on matplotlib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/radioshiny/multiaxes",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires='>=3.6',
+)
