4b3bd97999e9a3ca31d2e37f30531655c9c0e023
radioshiny <radioshiny@gmail.com>
2020-10-14 12:07:19 +0900

    add user defined figure width

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="multiaxes", # Replace with your own username
-    version="0.1.8",
+    version="0.1.9",
     author="Shinyoung Kim",
     author_email="radioshiny@gmail.com",
     description="Multiaxes is a python class based on matplotlib",
