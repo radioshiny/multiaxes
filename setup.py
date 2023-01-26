176078226bd8c4bc90b68cd0f5cbcde79e0b8966
radioshiny <radioshiny@gmail.com>
2023-01-27 04:23:40 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="multiaxes", # Replace with your own username
-    version="0.3.0",
+    version="0.4.0",
     author="Shinyoung Kim",
     author_email="radioshiny@gmail.com",
     description="Multiaxes is a python class based on matplotlib",
